3b31ee08bffef2228c262e6402e43ad0ec8f39f9
Student <student@macbook-air.lan>
2024-08-23 10:03:57 -0700

    Initial commit

---
+++ src/test/java/com/example/jenkins_tests/simple/DisabledTests.java
@@ -0,0 +1,21 @@
+package com.example.jenkins_tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class DisabledTests {
+        @Test
+        @Disabled
+        void test1(){
+            assertTrue(false);
+        }
+        @Test
+        @Disabled("test sucks")
+        void test2(){
+            assertTrue(false);
+        }
+    }
+
